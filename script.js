11d5a47cedf081b8dfff29aa1bc25eb7397455e7
Fernando <59489587+fercodes@users.noreply.github.com>
2026-05-10 22:56:49 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  if (operator === '+') return add(a, b);
+  if (operator === '-') return subtract(a, b);
+  if (operator === '*') return multiply(a, b);
+  if (operator === '/') return divide(a, b);
+}
